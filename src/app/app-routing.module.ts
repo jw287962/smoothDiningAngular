0715cae759c0020b288464545009c8438bebe4da
Jason <jasonwong287962@gmail.com>
2023-07-04 01:30:32 -0500

    routing redirect to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'home', // This is the original '/home' route
+    component: HomeComponent,
+  },
+  {
+    path: '', // Empty path, this will be the new default route
+    redirectTo: 'home', // Redirect to the original '/home' route
+    pathMatch: 'full', // Make sure the path matches exactly
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NavComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
