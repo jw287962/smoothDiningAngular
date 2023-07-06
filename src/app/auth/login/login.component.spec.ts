d03cfb687ecf36bf02fa6bb1d40d79dede0dfcff
Jason <jasonwong287962@gmail.com>
2023-07-06 01:12:13 -0500

    fix tests

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormErrorComponent } from 'src/app/form-error/form-error.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
@@ -9,7 +10,7 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ReactiveFormsModule],
-      declarations: [LoginComponent],
+      declarations: [LoginComponent, FormErrorComponent],
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
