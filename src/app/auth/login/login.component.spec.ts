214e6703f4e0ccdb79873018b13aad221a2c234f
Jason <jasonwong287962@gmail.com>
2023-07-07 02:38:14 -0500

    fix tests

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -3,13 +3,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { FormErrorComponent } from 'src/app/auth/form-error/form-error.component';
+import { StoreModule } from '@ngrx/store';
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule],
+      imports: [ReactiveFormsModule, StoreModule.forRoot({})],
       declarations: [LoginComponent, FormErrorComponent],
     });
     fixture = TestBed.createComponent(LoginComponent);
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { LoginApiService } from 'src/store/service/login.service';
+
+import { Injectable } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+
+@Injectable()
+export class MockLoginApiService {
+  // Implement mock methods and properties as needed
+  fetchStores() {
+    return this.manageError();
+  }
+  manageError() {
+    return [];
+  }
+}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +24,9 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      providers: [{ provide: LoginApiService, useClass: MockLoginApiService }],
       declarations: [HomeComponent],
+      imports: [StoreModule.forRoot({})],
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
