e54dc6882259a2b9410cf0b3ae9d804db1504893
Jason <jasonwong287962@gmail.com>
2023-07-07 11:57:28 -0500

    remove appRouter in secondary module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from '../app-routing.module';
+// import { AppRoutingModule } from '../app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { FormErrorComponent } from './form-error/form-error.component';
 @NgModule({
   declarations: [LoginComponent, FormErrorComponent],
-  imports: [CommonModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class AuthModule {}
