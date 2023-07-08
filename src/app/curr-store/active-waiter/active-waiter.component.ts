950ad6210590e31ca63a393132cf69ece3d80387
Jason <jasonwong287962@gmail.com>
2023-07-07 23:44:15 -0500

    made empty active waiter component

---
+++ src/app/curr-store/active-waiter/active-waiter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-waiter',
+  templateUrl: './active-waiter.component.html',
+  styleUrls: ['./active-waiter.component.css']
+})
+export class ActiveWaiterComponent {
+
+}
--- src/app/curr-store/curr-store.module.ts
+++ src/app/curr-store/curr-store.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreViewComponent } from './store-view/store-view.component';
+import { ActiveWaiterComponent } from './active-waiter/active-waiter.component';
 
 
 
 @NgModule({
   declarations: [
-    StoreViewComponent
+    StoreViewComponent,
+    ActiveWaiterComponent
   ],
   imports: [
     CommonModule
