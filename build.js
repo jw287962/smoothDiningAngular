04e99c1087afefb64b35c95c35b44b47853eb41f
Jason <jasonwong287962@gmail.com>
2023-07-08 00:19:56 -0500

    fix fscopy error

--- build.js
+++ build.js
@@ -2,6 +2,6 @@ const fs = require("fs-extra");
 
 const { execSync } = require("child_process");
 console.log("preparing build");
-const distBase = "dist/smooth-dining-angular/";
-fs.copySync("./src/404.html", `${distBase}404.html`);
-fs.copySync("./src/_redirects", `${distBase}`);
+const distBase = "dist/smooth-dining-angular";
+fs.copySync("./src/404.html", `${distBase}/404.html`);
+fs.copySync("./src/_redirects", `${distBase}/_redirects`);
