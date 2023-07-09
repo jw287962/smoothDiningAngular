18f8f8d825ca7c77a487dfbfd4102d6554eee7f8
Jason <jasonwong287962@gmail.com>
2023-07-09 17:57:36 -0500

    fix tests

--- src/app/curr-store/store-home/store-home.component.spec.ts
+++ src/app/curr-store/store-home/store-home.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StoreHomeComponent } from './store-home.component';
+import { StoreModule } from '@ngrx/store';
+import { CurrStoreRoutingModule } from '../curr-store-routing.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('StoreHomeComponent', () => {
   let component: StoreHomeComponent;
@@ -8,7 +11,8 @@ describe('StoreHomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [StoreHomeComponent]
+      declarations: [StoreHomeComponent],
+      imports: [StoreModule.forRoot(), AppRoutingModule],
     });
     fixture = TestBed.createComponent(StoreHomeComponent);
     component = fixture.componentInstance;
