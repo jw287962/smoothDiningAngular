0b403d26bda303f5eff85d35207179ac21c22202
Jason <jasonwong287962@gmail.com>
2023-07-09 01:32:37 -0500

    add basic store view datas/form

--- src/app/curr-store/store-view/store-view.component.ts
+++ src/app/curr-store/store-view/store-view.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import {
+  State,
+  activeStore,
+  selectStoreData,
+} from 'src/store/reducers/auth.reducer';
+import { Moment } from 'moment';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-store-view',
   templateUrl: './store-view.component.html',
-  styleUrls: ['./store-view.component.css']
+  styleUrls: ['./store-view.component.css'],
 })
 export class StoreViewComponent {
+  private _date: string = moment().format('yyyy-MM-DD');
+  storeData$: Observable<activeStore> = this._store.select(selectStoreData);
 
+  constructor(private _store: Store<State>) {}
+
+  date() {
+    return this._date;
+  }
+  handleViewDate() {
+    console.log(this._date);
+    this._date;
+  }
+
+  handleDateUpdate(e: any) {
+    this._date = moment(`${e.target.value}`).format('yyyy-MM-DD');
+  }
 }
