8e64a75bc9e68e149a3df0734cf9c59294bd394f
Jason <jasonwong287962@gmail.com>
2023-07-09 17:26:24 -0500

    new day view component for day-today work

---
+++ src/app/curr-store/day-view/day-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-view',
+  templateUrl: './day-view.component.html',
+  styleUrls: ['./day-view.component.css']
+})
+export class DayViewComponent {
+
+}
