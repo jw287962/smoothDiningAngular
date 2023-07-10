2c7ffa74c72d84d8a999e013d6f49e0b1422f4d2
Jason <jasonwong287962@gmail.com>
2023-07-10 01:13:35 -0500

    fix test

--- src/app/curr-store/store-home/store-view/store-view.component.spec.ts
+++ src/app/curr-store/store-home/store-view/store-view.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StoreViewComponent } from './store-view.component';
 import { StoreModule } from '@ngrx/store';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('StoreViewComponent', () => {
   let component: StoreViewComponent;
@@ -10,7 +11,7 @@ describe('StoreViewComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [StoreViewComponent],
-      imports: [StoreModule.forRoot()],
+      imports: [StoreModule.forRoot(), ReactiveFormsModule],
     });
     fixture = TestBed.createComponent(StoreViewComponent);
     component = fixture.componentInstance;
