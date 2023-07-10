996eb936292d964438d40db0eb42e09c14c6b089
Jason <jasonwong287962@gmail.com>
2023-07-10 00:42:54 -0500

    search filter works (to add to today's waiter)

--- src/app/curr-store/store-home/day-view/day-view.component.ts
+++ src/app/curr-store/store-home/day-view/day-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventType } from '@angular/router';
 import { StoreApiService } from 'src/store/service/store.service';
 import { waiterInterface } from 'src/store/service/types';
 @Component({
@@ -8,6 +9,9 @@ import { waiterInterface } from 'src/store/service/types';
 })
 export class DayViewComponent {
   waiters?: waiterInterface[];
+  filteredWaiter?: waiterInterface[];
+  searchName: string = '';
+  timeout?: any;
   constructor(private _storeService: StoreApiService) {
     this.fetchWaiters();
   }
@@ -15,7 +19,24 @@ export class DayViewComponent {
   async fetchWaiters() {
     const result = await this._storeService.fetchWaiters();
     this.waiters = result;
+    this.filteredWaiter = result;
   }
 
   async ngOnInit() {}
+
+  processChange(e: Event) {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+    this.timeout = setTimeout(() => {
+      this.filteredWaiter = this.waiters?.filter((ele) => {
+        const search = (e?.target as HTMLInputElement).value;
+        return ele.name.toLowerCase().indexOf(search) != -1;
+      });
+    }, 300);
+  }
+
+  clickPerson(name: string) {
+    this.searchName = name;
+  }
 }
