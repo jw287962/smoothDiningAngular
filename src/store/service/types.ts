8b65b727abbea942e7077183a507d3d06ffe8497
Jason <jasonwong287962@gmail.com>
2023-07-09 19:35:42 -0500

    add interface for waiter model

---
+++ src/store/service/types.ts
@@ -0,0 +1,10 @@
+export interface waiterInterface {
+  name: string;
+  birthdate: Date;
+  preferences?: {
+    maxActiveTableForPermission: number;
+    waitToSitUntilEntreeOut: { min: number };
+  };
+  store: string;
+  status: Boolean;
+}
