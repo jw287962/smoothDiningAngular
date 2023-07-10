f6647b43be429f674478e8a2c14af2927cf6d155
Jason <jasonwong287962@gmail.com>
2023-07-10 01:58:15 -0500

    add basic waiterActiveRowContainer component

---
+++ src/app/curr-store/store-home/day-view/waiter-active-row/waiter-active-row.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WaiterActiveRowComponent } from './waiter-active-row.component';
+
+describe('WaiterActiveRowComponent', () => {
+  let component: WaiterActiveRowComponent;
+  let fixture: ComponentFixture<WaiterActiveRowComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [WaiterActiveRowComponent]
+    });
+    fixture = TestBed.createComponent(WaiterActiveRowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/curr-store/store-home/day-view/waiter-active-row/waiter-active-row.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waiter-active-row',
+  templateUrl: './waiter-active-row.component.html',
+  styleUrls: ['./waiter-active-row.component.css']
+})
+export class WaiterActiveRowComponent {
+
+}
