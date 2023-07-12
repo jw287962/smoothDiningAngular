a57221623134ad3d34a0238afbc940d4ff6e5c2a
Jason <jasonwong287962@gmail.com>
2023-07-12 17:12:47 -0500

    handleResponse helper

--- src/store/service/types.ts
+++ src/store/service/types.ts
@@ -15,3 +15,12 @@ export const cookieOptions: CookieOptions = {
   secure: true, // Mark the cookie as secure
   sameSite: 'None', // Set SameSite attribute to None
 };
+
+type Result = {
+  error: string;
+  message: string;
+  result: any;
+};
+export const handleResponseBody = (result: Result): any => {
+  return result.error || result.result || result.message;
+};
