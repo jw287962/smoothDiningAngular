f8a5b4c192d0b8d4827ab33dd73101c43c547f18
Jason <jasonwong287962@gmail.com>
2023-07-13 21:12:08 -0500

    added fillers for basic design of workflow

--- src/app/curr-store/store-home/day-view/waiter-active-row/waiter-active-row.component.ts
+++ src/app/curr-store/store-home/day-view/waiter-active-row/waiter-active-row.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { shiftInterface } from 'src/store/service/types';
+import { shiftInterface, waiterInterface } from 'src/store/service/types';
 
 @Component({
   selector: 'app-waiter-active-row',
@@ -7,8 +7,21 @@ import { shiftInterface } from 'src/store/service/types';
   styleUrls: ['./waiter-active-row.component.css'],
 })
 export class WaiterActiveRowComponent {
-  @Input() waiter: any;
-  constructor() {}
+  @Input() waiter!: shiftInterface;
+  @Input() showShifts: boolean = false;
+  constructor() {
+    // console.log(this.waiter);
+    // console.log(this.waiter?.waiter?.[0]);
+  }
 
-  ngInIt() {}
+  ngOnInit() {
+    console.log(this.showShifts);
+    // this.waiter.shiftTables.fill(1, this.waiter.shiftTables.length, 20);
+    const currLength = this.waiter.shiftTables.length;
+    for (let i = currLength; i < 30; i++) {
+      this.waiter.shiftTables.push({});
+    }
+
+    console.log(this.waiter);
+  }
 }
