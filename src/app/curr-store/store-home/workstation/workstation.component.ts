6f3c30d91d98f5c2d298ae954636e365005de1a4
Jason <jasonwong287962@gmail.com>
2023-07-16 14:04:32 -0500

    time name and partySize

--- src/app/curr-store/store-home/workstation/workstation.component.ts
+++ src/app/curr-store/store-home/workstation/workstation.component.ts
@@ -108,4 +108,9 @@ export class WorkstationComponent {
       'will display party data  | maybe a new component with @input of data'
     );
   }
+
+  convertToLocalTime(string: any) {
+    const time = new Date(string).toLocaleTimeString();
+    return time.substring(0, time.length - 6) + time.substring(time.length - 2);
+  }
 }
